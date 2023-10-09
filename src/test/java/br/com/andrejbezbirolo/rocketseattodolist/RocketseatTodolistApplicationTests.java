4c3bf1d568b905d4a4418efc9738888af37fc388
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2023-10-09 16:54:06 -0300

    feat: inicialização do todo-list

---
+++ src/test/java/br/com/andrejbezbirolo/rocketseattodolist/RocketseatTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrejbezbirolo.rocketseattodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketseatTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
