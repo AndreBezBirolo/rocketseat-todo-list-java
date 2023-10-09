606d3ab5b0871ca506d09d281ab7803d9911e007
André J. Bez Birolo <andrecentrobrasileiro@outlook.com>
2023-10-09 17:21:00 -0300

    feat: adição do controlador de usuário

--- src/main/java/br/com/andrejbezbirolo/rocketseattodolist/RocketseatTodolistApplication.java
+++ src/main/java/br/com/andrejbezbirolo/rocketseattodolist/RocketseatTodolistApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RocketseatTodolistApplication {
-
+ 
     public static void main(String[] args) {
         SpringApplication.run(RocketseatTodolistApplication.class, args);
     }
---
+++ src/main/java/br/com/andrejbezbirolo/rocketseattodolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.andrejbezbirolo.rocketseattodolist.user;
+
+public class UserModel {
+
+    public String username;
+    public String name;
+    public String password;
+}
